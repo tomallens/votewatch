c8d301cb1c1c02ec124b808755a53a3e47e72b7f
Ali <abhatti89@gmail.com>
2022-11-23 15:18:58 +0000

    Delete example memberId and commented out lines

--- api/divisionsClient.js
+++ api/divisionsClient.js
@@ -1,9 +1,7 @@
-// const fetch = require("node-fetch");
-// import fetch from 'node-fetch';
 class DivisionsClient {
   constructor(memberId) {
-    this.memberId = 4132;
-    this.apiURL = `https://commonsvotes-api.parliament.uk/data/divisions.json/membervoting?memberId=${memberId}`;
+    this.memberId = memberId;
+    this.apiURL = `https://commonsvotes-api.parliament.uk/data/divisions.json/membervoting?memberId=${this.memberId}`;
   }
   getVotesFromApi(callback) {
     fetch(this.apiURL)
@@ -13,35 +11,5 @@ class DivisionsClient {
       });
   }
 }
-
-
-    // commonsvotes-api.parliament.uk/data/divisions.json/membervoting?memberId=3977
-  
-
-
-        // fetch(this._fetchUrl, {
-        //     method: 'GET',
-        //     headers: {
-        //       Accept: 'application/json',
-        //       'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({
-        //       memberId: this.memberId
-        //     })
-        //   });
-		// try {
-		// 	let response = await fetch(this._fetchUrl + `?memberId=${this.memberId}`);
-		// 	let json = await response.json();
-		// 	return json;
-		// } catch (error) {
-		// 	console.error(error);
-		// }
-	
-
-   
-
-	// queryParameters.memberId()
-	// memberId=3977&take=3
-
 
 module.exports = DivisionsClient;
