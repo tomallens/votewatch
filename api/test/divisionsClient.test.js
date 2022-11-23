c6d066ac0dbfbe9cb2a42fd4f23828533b9e2316
Ali <abhatti89@gmail.com>
2022-11-23 15:02:55 +0000

    Modify initial unit test for DivisionsClient

--- api/test/divisionsClient.test.js
+++ api/test/divisionsClient.test.js
@@ -2,19 +2,37 @@ const DivisionsClient = require('../divisionsClient');
 const fakeApiResponse = require('./fakeApiResponse.json');
 require('jest-fetch-mock').enableMocks();
 
-describe('Client class', () => {
+describe("DivisionsClient Api class", () => {
   beforeEach(() => {
     fetch.resetMocks();
   });
-  it('calls fetch and loads data', (done) => {
-    fakeMemberId = 0 
-    const divisionsClient = new DivisionsClient(fakeMemberId);
-
-    fetch.mockResponse(JSON.stringify(fakeApiResponse));
-
-    divisionsClient.getVotesFromApi((returnedDataFromApi) => {
-      expect(returnedDataFromApi).toEqual(fakeApiResponse); // Asessing deep equality
+  it("calls fetch and loads data", (done) => {
+    const client = new DivisionsClient();
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        name: ["Boris Johnson"],
+      })
+    );
+    client.getVotesFromApi((callback) => {
+      console.log(callback);
+      expect(callback.name).toEqual(["Boris Johnson"]);
       done();
     });
   });
 });
+
+
+  // it('calls fetch and loads data', (done) => {
+  //   fakeMemberId = 0
+  //   const divisionsClient = new DivisionsClient(fakeMemberId);
+  //   divisionsClient._fetchUrl = 
+    
+  //   fetch.mockResponse(JSON.stringify(fakeApiResponse));
+
+  //   divisionsClient.getVotesFromApi((returnedDataFromApi) => {
+  //     console.log(returnedDataFromApi)
+  //     expect(returnedDataFromApi).toEqual(fakeApiResponse); // Asessing deep equality
+  //     done();
+  //   });
+  // });
+
