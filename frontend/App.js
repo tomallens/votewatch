67e38718d31767700d1d5817b982ac6b3d0aff87
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-24 15:25:33 +0000

    Add fetch for second API and display MP member ID and picture on page

--- frontend/App.js
+++ frontend/App.js
@@ -4,8 +4,21 @@ import { StyleSheet, Image, Text, View } from 'react-native';
 
 export default function App() {
   const [isLoading, setLoading] = useState(true);
+  const [isLoadingMp, setLoadingMp] = useState(true);
   const [data, setData] = useState([]);
+  const [mpData, setMpData] = useState([]);
   console.log(data);
+  console.log('MP data: ', mpData);
+
+  useEffect(() => {
+    fetch(
+      `https://members-api.parliament.uk/api/Members/Search?Name=Boris%20Johnson`
+    )
+      .then((response) => response.json())
+      .then((json) => setMpData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoadingMp(false));
+  }, []);
 
   useEffect(() => {
     fetch(
@@ -19,19 +32,28 @@ export default function App() {
 
   return (
     <View style={{ flex: 1, padding: 24 }}>
-      {isLoading ? (
+      {isLoading || isLoadingMp ? (
         <Text>Loading...</Text>
       ) : (
         <View style={styles.container}>
           <Text>
+            <Image
+              source={{
+                uri: `${mpData.items[0].value.thumbnailUrl}`,
+                width: 60,
+                height: 60,
+              }}
+            />
+            <Text>{`${mpData.items[0].value.id}\n`}</Text>
+
             {data.map((individualData) => {
-              return `Date: ${
+              return `\nDate: ${
                 individualData.PublishedDivision.Date
               }\nDivision id: ${
                 data[0].PublishedDivision.Date
               }\nDivision title: ${data[0].PublishedDivision.Title}\nVoted: ${
                 !!data[0].MemberVotedAye ? 'Yes' : 'No'
-              }\n\n`;
+              }\n`;
             })}
           </Text>
           {/* <Text>{`${data.items[0].value.id}`}</Text>
