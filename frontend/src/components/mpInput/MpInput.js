6a94c2286130d0257f19351e5139d8f83e32b04c
Chris <caston88@gmail.com>
2022-11-24 17:37:35 +0000

    Add submission feature for MpInput

--- frontend/src/components/mpInput/MpInput.js
+++ frontend/src/components/mpInput/MpInput.js
@@ -1,19 +1,28 @@
 import React from "react";
-import { View, StyleSheet, TextInput } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export const MpInput = () => {
   const [mpName, onChangeMpName] = React.useState("");
 
+ 
   return (
     <View>
       <TextInput
         style={styles.input}
-        onChangeText={onChangeMpName}
-        value={mpName}
         placeholder="Enter MP name"
+        onSubmitEditing={(value) => onChangeMpName(value.nativeEvent.text)}
       />
+      <Text>It is { mpName }</Text>  
+      
     </View>
   );
+
 };
 
 const styles = StyleSheet.create({
@@ -22,5 +31,18 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
+  },
+  buttonText: {
+    color: "white",
+  },
+  button: {
+    backgroundColor: "black",
+    alignItems: "center",
+    borderRadius: 20,
+    borderBottomRightRadius: 20,
+    height: 40,
+    justifyContent: "center",
+    margin: -1,
+    paddingHorizontal: 20,
   },
 });
