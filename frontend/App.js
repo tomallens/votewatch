b0c8e3b9d90673eda5180fb16189640f0ed66c3d
Chris <caston88@gmail.com>
2022-11-24 21:50:42 +0000

    Add fetch for mp divisions and ID

--- frontend/App.js
+++ frontend/App.js
@@ -1,14 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
-import { MpInput } from './src/components/mpInput/MpInput';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Image, Text, View, SafeAreaView } from "react-native";
+import { MpInput } from "./src/components/mpInput/MpInput";
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [isLoadingMp, setLoadingMp] = useState(true);
+  const [data, setData] = useState([]);
+  const [mpData, setMpData] = useState([]);
+
+  useEffect(() => {
+    handleFeed();
+  }, []);
+
+  async function handleFeed() {
+    const memberId = await getMpId();
+    await getDivisions(memberId);
+  }
+
+  async function getMpId() {
+    return fetch(
+      `https://members-api.parliament.uk/api/Members/Search?Name=Munira%20Wilson`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setMpData(json);
+        setLoadingMp(false);
+        return json.items[0].value.id;
+      });
+  }
+
+  async function getDivisions(memberId) {
+    fetch(
+      `https://commonsvotes-api.parliament.uk/data/divisions.json/membervoting?memberId=${memberId.toString()}`
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Welcome to Votewatch</Text>
       <Text>Keeping eyes on the ayes</Text>
       <StatusBar style="auto" />
-      <MpInput/>
+      <MpInput />
+      <View style={{ flex: 1, padding: 24 }}>
+        {isLoading ? (
+          <Text>Loading...</Text>
+        ) : (
+          <View style={styles.container}>
+            <Text>
+              <Image
+                source={{
+                  uri: `${mpData.items[0].value.thumbnailUrl}`,
+                  width: 60,
+                  height: 60,
+                }}
+              />
+              <Text>{`${mpData.items[0].value.id}\n`}</Text>
+
+              {data.map((individualData) => {
+                return `\nDate: ${
+                  individualData.PublishedDivision.Date
+                }\nDivision id: ${
+                  individualData.PublishedDivision.Date
+                }\nDivision title: ${
+                  individualData.PublishedDivision.Title
+                }\nVoted: ${!!individualData.MemberVotedAye ? "Yes" : "No"}\n`;
+              })}
+            </Text>
+            <StatusBar style="auto" />
+          </View>
+        )}
+      </View>
     </SafeAreaView>
   );
 }
@@ -16,8 +82,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
