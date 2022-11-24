125d5215ef3f2e21377865bffb846e45aaf1df89
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-24 12:13:03 +0000

    Modify App to retrieve data from API and display it on the page

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  console.log(data);
+
+  useEffect(() => {
+    fetch(
+      `https://commonsvotes-api.parliament.uk/data/divisions.json/membervoting?memberId=4494`
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <View style={styles.container}>
+          <Text>{`${data[5].MemberId}`}</Text>
+          {/* <Image source={`${data[0].MemberId}`} /> */}
+          <StatusBar style="auto" />
+        </View>
+      )}
     </View>
   );
 }
