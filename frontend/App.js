f56c9eb493ed6529c2781f65dfd8258a01edc3ca
Ali <abhatti89@gmail.com>
2022-11-25 12:01:08 +0000

    Template email button added

--- frontend/App.js
+++ frontend/App.js
@@ -1,50 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Image, Text, View } from 'react-native';
+import { StyleSheet, Image, Text, View, Linking, Button } from 'react-native';
 
 export default function App() {
   const [isLoading, setLoading] = useState(true);
   const [isLoadingMp, setLoadingMp] = useState(true);
   const [data, setData] = useState([]);
   const [mpData, setMpData] = useState([]);
-  console.log('divisions data:', data);
-  console.log('MP data: ', mpData);
-  var memberId = '4494';
 
   // function updateMemberId() {
   //   await memberId = mpData.items[0].value.id;
   // }
 
-  Promise.resolve(
-    useEffect(() => {
-      fetch(
-        `https://members-api.parliament.uk/api/Members/Search?Name=Boris%20Johnson`
-      )
-        .then((response) => response.json())
-        .then((json) => {
-          setMpData(json);
-          console.log('data id from json', json.items[0].value.id);
-          memberId = json.items[0].value.id;
-          console.log('member id', memberId);
-        })
-        .catch((error) => console.error(error))
-        .finally(() => {
-          setLoadingMp(false);
-          console.log('memberId end of function', memberId);
-          return memberId;
-        });
-    }, [])
-  ).then(
-    useEffect(() => {
-      fetch(
-        `https://commonsvotes-api.parliament.uk/data/divisions.json/membervoting?memberId=${memberId.toString()}`
-      )
-        .then((response) => response.json())
-        .then((json) => setData(json))
-        .catch((error) => console.error(error))
-        .finally(() => setLoading(false));
-    }, [])
-  );
+  useEffect(() => {
+    feedHandler()
+  }, [])
+  
+  async function feedHandler() {
+    const memberId = await getMpId()
+    await getDivisions(memberId)
+  }
+  
+  async function getMpId() {
+    return fetch(
+      `https://members-api.parliament.uk/api/Members/Search?Name=Catherine%20West`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setMpData(json)
+        setLoadingMp(false);
+
+        return json.items[0].value.id;
+      })
+  }
+
+  async function getDivisions(memberId){
+    fetch(
+      `https://commonsvotes-api.parliament.uk/data/divisions.json/membervoting?memberId=${memberId.toString()}`
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+
+  }
+  
 
   // useEffect(() => {
   //   fetch(
@@ -70,7 +70,10 @@ export default function App() {
                 height: 60,
               }}
             />
-            <Text>{`${mpData.items[0].value.id}\n`}</Text>
+              <Text>{` MP ID: ${mpData.items[0].value.id}\n`}</Text>
+              
+              <Button onPress={() => Linking.openURL('mailto:support@example.com?subject=SendMail&body=Description')}
+                title="Email this MP" />          
 
             {data.map((individualData) => {
               return `\nDate: ${
@@ -81,7 +84,8 @@ export default function App() {
                 individualData.PublishedDivision.Title
               }\nVoted: ${!!individualData.MemberVotedAye ? 'Yes' : 'No'}\n`;
             })}
-          </Text>
+            </Text>
+                        
           {/* <Text>{`${data.items[0].value.id}`}</Text>
           <Image
             source={{
