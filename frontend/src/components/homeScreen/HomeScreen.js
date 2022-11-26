98046ec9162976db20a24f7dac355de7741c382b
Chris <caston88@gmail.com>
2022-11-26 14:02:00 +0000

    Add navigation to Feed from Homepage

--- frontend/src/components/homeScreen/HomeScreen.js
+++ frontend/src/components/homeScreen/HomeScreen.js
@@ -1,13 +1,17 @@
 import * as React from "react";
 import { Button, View, Text } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useNavigation } from "@react-navigation/native";
 
 export function HomeScreen() {
+  const navigation = useNavigation();
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Welcome to Votewatch</Text>
       <Text>Keeping eyes on the ayes</Text>
+      <Button
+        title="Watch votes now!"
+        onPress={() => navigation.navigate("Feed")}
+      />
     </View>
   );
 }
