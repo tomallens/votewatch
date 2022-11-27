838aafad1368055d0024aa9ee10314606d22a079
Tom <tomoliverallen@gmail.com>
2022-11-27 12:13:32 +0000

    Added Push Token Registration

--- frontend/App.js
+++ frontend/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
+import * as Notifications from 'expo-notifications';
 
 import { StyleSheet, Image, Text, View, SafeAreaView } from "react-native";
 import { MpInput } from "./src/components/mpInput/MpInput";
@@ -12,7 +13,28 @@ export default function App() {
 
   useEffect(() => {
     callCommonsApi();
+    registerForPushNotificationsAsync();
   }, [mpName]);
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+  
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+  
+    if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  
+    return token;
+  }
 
   async function callCommonsApi() {
     if (mpName == "") return;
---
+++ frontend/Notifier.js
@@ -0,0 +1,22 @@
+import * as Notifications from 'expo-notifications';
+
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+
+  if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+  }
+  if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+  }
+  token = (await Notifications.getExpoPushTokenAsync()).data;
+  console.log(token);
+
+  return token;
+}
