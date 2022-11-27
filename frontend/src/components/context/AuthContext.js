77ff08165dee3649191605ce25d7aa26a652b921
Chris <caston88@gmail.com>
2022-11-27 15:48:24 +0000

    Add token storage

--- frontend/src/components/context/AuthContext.js
+++ frontend/src/components/context/AuthContext.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const AuthContext = createContext();
 
@@ -8,14 +9,33 @@ export const AuthProvider = ({ children }) => {
   const [userToken, setUserToken] = useState(null);
 
   const login = () => {
+    setIsLoading(true);
     setUserToken("cuddlezzzzz");
+    AsyncStorage.setItem("userToken", "cuddlezzzzz");
     setIsLoading(false);
   };
 
   const logout = () => {
+    setIsLoading(true);
     setUserToken(null);
+    AsyncStorage.removeItem("userToken", userToken);
     setIsLoading(false);
   };
+
+  const isLoggedIn = async () => {
+    try {
+      setIsLoading(true);
+      let userToken = AsyncStorage.getItem("userToken");
+      setUserToken(userToken);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(`Logged in error ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    isLoggedIn();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ login, logout, isLoading, userToken }}>
