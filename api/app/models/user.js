7c7dd0f030840d4c1b6a36520973e8d45aec8b8f
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-28 15:38:44 +0000

    Add files to handle backend user auth with sequelize

---
+++ api/app/models/user.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define('user', {
+    username: {
+      type: Sequelize.STRING,
+    },
+    email: {
+      type: Sequelize.STRING,
+    },
+    password: {
+      type: Sequelize.STRING,
+    },
+  });
+
+  return User;
+};
---
+++ api/app/routes/division-routes.js
@@ -0,0 +1,28 @@
+const { jwtAuth } = require('../middleware');
+const productServices = require('../services/division-services.js');
+
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Authorization',
+      'Origin, Content-Type, Accept'
+    );
+    next();
+  });
+
+  // add a product
+  //   app.post("/api/auth/addproduct", [jwtAuth.verifyToken], productServices.create);
+
+  // find all product
+  //   app.get("/api/auth/getproducts", [jwtAuth.verifyToken], productServices.findAll);
+
+  // find product by product id
+  //   app.post("/api/auth/getproductbyid", [jwtAuth.verifyToken], productServices.findOne);
+
+  // update product by product id
+  //   app.post("/api/auth/update", [jwtAuth.verifyToken], productServices.update);
+
+  // delete productby product id
+  //   app.post("/api/auth/delete", [jwtAuth.verifyToken], productServices.delete);
+};
