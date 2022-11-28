5c8dd51e9e14e2f48a928a255c77cdeecf5a77a0
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-28 16:39:56 +0000

    Remove unused routes

--- api/app/services/division-services.js
+++ api/app/services/division-services.js
@@ -1,8 +1,8 @@
 const db = require('../models');
-const Product = db.product;
+const Division = db.division;
 // const Op = db.Sequelize.Op;
 
-// Add new product
+// Add new division
 // exports.create = (req, res) => {
 //   console.log("Request : ", req.body)
 //   validateRequest(req);
@@ -18,107 +18,7 @@ const Product = db.product;
 // //     .catch(err => {
 // //       res.status(500).send({
 // //         message:
-// //           err.message || "Error when add a product!"
+// //           err.message || "Error when add a division!"
 // //       });
 // //     });
 // };
-
-// // Find all products
-// exports.findAll = (req, res) => {
-//   Product.findAll({ where: {isDeleted: false} })
-//     .then(data => {
-//       res.send(data);
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message:
-//           err.message || "Error when get all product!"
-//       });
-//     });
-// };
-
-// // Find product by product id
-// exports.findOne = (req, res) => {
-//   console.log("Request : ", req.body)
-//   validateRequest(req);
-
-//   const id = req.body.id;
-//   Product.findByPk(id)
-//     .then(data => {
-//       if (data) {
-//         res.send(data);
-//       } else {
-//         res.status(404).send({
-//           message: "Product not found!"
-//         });
-//       }
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message: "Error when get product by product id : " + id
-//       });
-//     });
-// };
-
-// // Update product by product id
-// exports.update = (req, res) => {
-//   console.log("Request : ", req.body)
-//   validateRequest(req);
-
-//   const id = req.body.id;
-//   Product.update(req.body, {
-//     where: { id: id }
-//   })
-//     .then(num => {
-//       if (num == 1) {
-//         res.send({
-//           message: "Product successfully updated."
-//         });
-//       } else {
-//         res.send({
-//           message: "Update process was failedQ"
-//         });
-//       }
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message: "Error updating product with product id : " + id
-//       });
-//     });
-// };
-
-// // Delete product by product id
-// exports.delete = (req, res) => {
-//   console.log("Request : ", req.body)
-//   validateRequest(req);
-
-//   const id = req.body.id;
-//   Product.destroy({
-//     where: { id: id }
-//   })
-//     .then(num => {
-//       if (num == 1) {
-//         res.send({
-//           message: "Product successfully deleted."
-//         });
-//       } else {
-//         res.send({
-//           message: "Delete process was failed!"
-//         });
-//       }
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message: "Couldn't delete product with product id : " + id
-//       });
-//     });
-// };
-
-// function validateRequest(req){
-//   if (!req.body) {
-//     res.status(400).send({
-//       message: "Request is empty!"
-//     });
-//     return;
-//   }
-// }
