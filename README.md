c3102eecd756d8add03a2c2600be2e85f74dac38
Chris <caston88@gmail.com>
2022-11-30 20:26:55 +0000

    Modify gitignore

--- README.md
+++
@@ -1,9 +0,0 @@
-## Development use:
-```
-nvm install 16
-nvm use 16
-cd votewatch
-npm install
-npm start
-```
-
--- frontend/src/navigation/AppStack.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-import TabNavigator from "./TabNavigator";
-
-function AppStack() {
-  return <TabNavigator />;
-}
-
-export default AppStack;
